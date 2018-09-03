7b369cb01e25a1ff052d794590f919c4f8274b44
Colin Osborn <colin.osborn@gmail.com>
2018-09-03 12:48:03 -0600

    initial theme setup

---
+++ wp-content/themes/scandictheme/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Scandinavian Greats</title>
+</head>
+<body>
+    <p>Welcome to the new theme</p>
+</body>
+</html>
